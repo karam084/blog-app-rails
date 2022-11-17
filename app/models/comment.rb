c33530c5af69bf5f23718053ebaa37758549ffe7
karam084 <karam.ezz84@gmail.com>
2022-11-17 02:36:54 +0200

    Add intigration and spec to features

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,22 +1,9 @@
-class Post < ApplicationRecord
+class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
-  after_save :updates_posts_counter
+  belongs_to :post
+  after_save :update_comments_counter
 
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
-  def updates_posts_counter
-    author.update(posts_counter: author.posts.count)
-  end
-
-  def recent_comments
-    comments.limit(5).order(created_at: :desc)
-  end
-
-  def all_comments
-    comments.all
+  def update_comments_counter
+    post.increment!(:comments_counter)
   end
 end
