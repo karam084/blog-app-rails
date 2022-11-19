3a285bda6f159334c7cc89570344d131fa37339f
karam084 <karam.ezz84@gmail.com>
2022-11-19 22:44:32 +0200

    Add user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user_admin!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  private
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(_resource_or_scope)
+    root_path
   end
 end
